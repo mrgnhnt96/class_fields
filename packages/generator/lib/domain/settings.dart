2e9dba8c0f9accd11eda02c32c9b40a3f5545278
Morgan Hunt <mrgnhnt96@gmail.com>
2022-01-11 14:41:57 -0700

    feat: add debug & _toJson methods for testing

--- packages/generator/lib/domain/settings.dart
+++ packages/generator/lib/domain/settings.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 /// {@template settings}
 /// The configuration from the build.yaml file
 /// {@endtemplate}
@@ -16,4 +18,16 @@ class Settings {
 
   /// Whether to format the output of the generated code.
   final bool formatOuput;
+
+  /// sets formatOutput to true
+  @visibleForTesting
+  static Map<String, dynamic> debug() {
+    return const Settings(formatOuput: true)._toJson();
+  }
+
+  Map<String, dynamic> _toJson() {
+    return <String, dynamic>{
+      'format_ouput': formatOuput,
+    };
+  }
 }
