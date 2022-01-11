0171e95feb0bb2dbcff700425695caa9b333d672
Morgan Hunt <mrgnhnt96@gmail.com>
2022-01-11 15:02:24 -0700

    feat: create field keys template

--- packages/generator/lib/templates/field_keys_template.dart
+++ packages/generator/lib/templates/field_keys_template.dart
@@ -1,5 +1,7 @@
 import 'package:field_keys/domain/class.dart';
+import 'package:field_keys/domain/field.dart';
 import 'package:field_keys/templates/template.dart';
+import 'package:field_keys/util/string_buffer_ext.dart';
 
 /// {@template field_keys_template}
 /// The template for the field keys
@@ -10,6 +12,27 @@ class FieldKeysTemplate extends Template {
 
   @override
   void generate(StringBuffer buffer) {
-    // TODO: implement generate
+    buffer.writeObject(
+      'class ${subject.genName}',
+      body: () {
+        buffer
+          ..writeln('const ${subject.genName}();')
+          ..writeln()
+          ..writeAll(subject.fields.instanciators, '\n')
+          ..writeln();
+      },
+    );
+  }
+}
+
+extension on Class {
+  String get genName {
+    return '_\$${name}FieldKeys';
+  }
+}
+
+extension on Iterable<Field> {
+  Iterable<String> get instanciators {
+    return map((field) => "final ${field.name} = '${field.name}';");
   }
 }
